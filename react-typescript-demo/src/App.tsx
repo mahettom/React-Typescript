76afc2600168abb5c390d8fd43b4ba31c89dc7bd
TomMahe <mahettom@gmail.com>
2023-06-22 16:08:52 +0200

    union of string litteral

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,39 +1,45 @@
 import './App.css'
-import { Greet } from './components/Greet'
-import { Person } from './components/Person'
-import { PersonList } from './components/PersonList'
+import { Status } from './components/Status'
+// import { Greet } from './components/Greet'
+// import { Person } from './components/Person'
+// import { PersonList } from './components/PersonList'
 
 function App() {
 
-  const personName = {
-    first: 'Christian',
-    last: 'Bale',
-  }
+  // const personName = {
+  //   first: 'Christian',
+  //   last: 'Bale',
+  // }
 
-  const nameList = [
-    {
-      first: 'John',
-      last: 'David Washigton',
-    },
-    {
-      first: 'Anya',
-      last: 'Taylor Joy',
-    },
-    {
-      first: 'Rami',
-      last: 'Malek',
-    },
-    {
-      first: 'Margot',
-      last: 'Robbie',
-    }
-  ]
+  // const nameList = [
+  //   {
+  //     first: 'John',
+  //     last: 'David Washigton',
+  //   },
+  //   {
+  //     first: 'Anya',
+  //     last: 'Taylor Joy',
+  //   },
+  //   {
+  //     first: 'Rami',
+  //     last: 'Malek',
+  //   },
+  //   {
+  //     first: 'Margot',
+  //     last: 'Robbie',
+  //   }
+  // ]
 
   return (
     <>
-      <Greet name='Tom' messageCount={10} isLoggedIn={false} />
+      {/* <Greet name='Tom' messageCount={10} isLoggedIn={false} />
       <Person name={personName}/>
-      <PersonList  names={nameList} />
+      <PersonList  names={nameList} /> */}
+
+      <Status status='loading' />
+
+
+
     </>
   )
 }
