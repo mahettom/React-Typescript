afe23c10a0a42b2603657319cf1f228ac291b736
TomMahe <mahettom@gmail.com>
2023-06-23 14:50:14 +0200

    passing Components as Children

--- Advanced-Props/src/App.tsx
+++ Advanced-Props/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { Heading } from './components/Heading'
 import { Status } from './components/Status'
+import { Oscar } from './components/Oscar'
 
 function App() {
 
   return (
     <>
       <Status status='loading' />
+      <Oscar>
+        <Heading>Oscar goes to Rami Malek!</Heading>
+      </Oscar>
     </>
   )
 }
---
+++ Advanced-Props/src/components/Oscar.tsx
@@ -0,0 +1,15 @@
+
+type OscarProps = {
+    children: React.ReactNode
+}
+
+
+
+
+export const Oscar = (props: OscarProps) => {
+
+
+  return (
+    <>{props.children}</>
+  )
+}
