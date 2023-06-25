1b0829ab9d17fa2d4032c811f027f40ecc62e080
TomMahe <mahettom@gmail.com>
2023-06-25 16:26:28 +0200

    create context folder

---
+++ useState-Hook/src/components/context/User.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+export const User = () => {
+
+    const handleLogin = () => {}
+    const handleLogout = () => {}
+
+  return (
+    <>
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleLogout}>Logout</button>
+        <div> User name is </div>
+        <div> User email is </div>
+    </>
+  )
+}
