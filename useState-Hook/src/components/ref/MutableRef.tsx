44359aea082dbd970dfa3795058ec57ca42c2ff6
TomMahe <mahettom@gmail.com>
2023-06-26 18:08:42 +0200

    useRef hook

---
+++ useState-Hook/src/components/ref/MutableRef.tsx
@@ -0,0 +1,31 @@
+import { useState, useRef, useEffect } from "react";
+
+
+
+export const MutableRef = () => {
+    
+    const [timer, setTimer] = useState(0)
+    const intervalRef = useRef(null)
+
+    const stopTimer = () => {
+        window.clearInterval(intervalRef.current)
+    }
+
+    useEffect(() => {
+
+      intervalRef.current = window.setInterval(() => {
+        setTimer((timer) => timer + 1)
+      }, 1000)
+
+      return () => {
+        stopTimer()
+      }
+    }, [])
+    
+    return (
+        <>
+            HookTimer - {timer} -
+            <button onClick={() => stopTimer}>Stop Time</button>
+        </>
+    )
+}
