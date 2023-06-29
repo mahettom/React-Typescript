7440fcaa7c1269eb64b04d132404dc45756aa6a9
TomMahe <mahettom@gmail.com>
2023-06-29 14:43:13 +0200

    Add comments to Custom Button

--- Advanced-Props/src/components/html/Button.tsx
+++ Advanced-Props/src/components/html/Button.tsx
@@ -11,3 +11,6 @@ export const CustomButton = ({variant, children, ...rest}: ButtonProps) => {
     <button className={`class-with-${variant}`} {...rest}>{children}</button>
   )
 }
+
+// & React.ComponentProps allow to acces the children and the rest of the props that was given without being to specific
+// { ...rest } -> spread the rest of the props given into the component that we gonna render
